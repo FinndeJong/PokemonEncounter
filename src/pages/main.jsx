3f8ac59cacd7927b144d3ba113801fd6973060f1
FinndeJong <70587533+FinndeJong@users.noreply.github.com>
2021-12-19 21:38:40 +0100

    update
    asdfasdf

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -27,32 +27,32 @@ const Main = () => {
 	return (
 		<>
 			<div className="Container">
-					{/* Counter */}
-					<div>
-						<h1>{Count}</h1>
-					</div>
-					{/* Buttons */}
-					<div>
-						<button className="button" onClick={() => setCount(Count + 1)}>+</button>
-						<button className="button" onClick={() => setCount(0)}>reset</button>
-						<button className="button" onClick={handleNext}>next</button>
-						<button className="button" onClick={() => setCount(Count - 1)}>-</button>
-					</div>
-					<br />
-					<div>
-						<button className="button" onClick={() => setCount(Count + 10)}>+10</button>
-						<button className="button" onClick={() => setCount(Count + 100)}>+100</button>
-						<button className="button" onClick={() => setCount(Count + 1000)}>+1000</button>
-						<button className="button" onClick={() => setCount(Count + 10000)}>+10000</button>
-					</div>
-					{/* Last Hunt */}
-					<div>
-						<h2>{LastHunt}</h2>
-					</div>
-					{/* History */}
-					{/* <div>
-						<button className="button" onClick={() => setHistory(true)}>History</button>
-					</div> */}
+				{/* Counter */}
+				<div>
+					<h1>{Count}</h1>
+				</div>
+				{/* Buttons */}
+				<div>
+					<button className="button" onClick={() => setCount(Count + 1)}>+</button>
+					<button className="button" onClick={() => setCount(0)}>reset</button>
+					<button className="button" onClick={handleNext}>next</button>
+					<button className="button" onClick={() => setCount(Count - 1)}>-</button>
+				</div>
+				<br />
+				<div>
+					<button className="button" onClick={() => setCount(Count + 10)}>+10</button>
+					<button className="button" onClick={() => setCount(Count + 100)}>+100</button>
+					<button className="button" onClick={() => setCount(Count + 1000)}>+1000</button>
+					<button className="button" onClick={() => setCount(Count + 10000)}>+10000</button>
+				</div>
+				{/* Last Hunt */}
+				<div>
+					<h2>{LastHunt}</h2>
+				</div>
+				{/* History */}
+				{/* <div>
+					<button className="button" onClick={() => setHistory(true)}>History</button>
+				</div> */}
 			</div>
 		</>
 	)
